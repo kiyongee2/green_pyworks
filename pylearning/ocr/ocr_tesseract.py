689b919c4f9d14f8f753d0ab4a0b97a629f20c1e
kiyongee2 <kiyongee2@gmail.com>
2023-05-23 08:02:44 +0900

    ocr

--- pylearning/ocr/ocr_tesseract.py
+++ pylearning/ocr/ocr_tesseract.py
@@ -8,14 +8,14 @@ from PIL import Image
 from pytesseract import pytesseract
 
 pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
-img_file = Image.open("images/cold.png")
+img_file = Image.open("source/간판.png")
 text = pytesseract.image_to_string(img_file, lang="kor")
-# print(text)
-print(text.replace(" ", ""))
+print(text)
+# print(text.replace(" ", ""))
 
 # 변환된 데이터를 파일에 쓰기
 # 인코딩(encoding) 속성 'utf8'
-with open("output/cold.txt", 'w', encoding='utf8') as f:
+with open("output/간판.txt", 'w', encoding='utf8') as f:
     # f.write(text)
     f.write("\n")
     f.write(text.replace(' ', ''))
